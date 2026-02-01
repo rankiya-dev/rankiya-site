ab230e660ced23796fc7975ed5222cf963e939f9
rankiya-dev <rankiya.app@gmail.com>
2026-02-01 19:03:17 +0530

    Update update.js

--- js/update.js
+++ js/update.js
@@ -3,7 +3,7 @@ let updateAvailable = false; // false कर दो तो update hide
 
 function checkAppInstalled(){
     let isInstalled = false;
-    let scheme = "intent://rankiya#Intent;scheme=rankiya;package=com.rankiya.app;end";
+    //let scheme = "intent://rankiya#Intent;scheme=rankiya;package=com.rankiya.app;end";
     let start = Date.now();
 
     window.location.href = scheme;
@@ -19,4 +19,5 @@ function checkAppInstalled(){
 }
 
 window.onload = checkAppInstalled;
+
 
