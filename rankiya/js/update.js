35a5cb816e508f57aed0176d69d2d05936edb44e
rankiya-dev <rankiya.app@gmail.com>
2026-02-01 12:31:01 +0530

    Add files via upload

---
+++ rankiya/js/update.js
@@ -0,0 +1,21 @@
+// ADMIN CONTROL
+let updateAvailable = true; // false कर दो तो update hide
+
+function checkAppInstalled(){
+    let isInstalled = false;
+    let scheme = "intent://rankiya#Intent;scheme=rankiya;package=com.rankiya.app;end";
+    let start = Date.now();
+
+    window.location.href = scheme;
+
+    setTimeout(function(){
+        if(Date.now() - start > 1500){
+            isInstalled = true;
+        }
+        if(isInstalled && updateAvailable){
+            document.getElementById("updateBar").style.display = "block";
+        }
+    },1200);
+}
+
+window.onload = checkAppInstalled;
